f2d6a3865037a908212459a08f56c378b9cc3c8a
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-02-06 23:42:27 -0700

    Implement search functionality using URL query params

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,6 @@
 import {defineQuery} from "next-sanity";
 
-export const PROJECTS_QUERY = defineQuery(`*[_type == "project" && defined(slug.current)] | order(_createdAt desc){
+export const PROJECTS_QUERY = defineQuery(`*[_type == "project" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
     _id,
     title,
     slug,
