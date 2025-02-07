9d891bd52bd31f4b70845c81a3b83cb42f06eb00
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-02-06 23:42:26 -0700

    Add project card component with example

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import ProjectCard from "@/components/ProjectCard";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query;
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: { _id: 1, name: 'Shivam Amin' },
+        _id: 1,
+        description: 'This is a description',
+        image: 'https://picsum.photos/350/200',
+        category: 'filler',
+        title: 'Sample Post',
+    }];
   return (
     <>
         <section className="pink_container">
@@ -13,6 +24,20 @@ export default async function Home({ searchParams }: {
                 Explore my projects and the technology behind them.
             </p>
             <SearchForm query={query} />
+        </section>
+        <section className="section_container">
+            <p className="text-30-semibold">
+                {query ? `Search results for "${query}"` : "All projects"}
+            </p>
+            <ul className="mt-7 card_grid">
+                {posts?.length > 0 ? (
+                    posts.map((post: ProjectCardType, index: number) => (
+                        <ProjectCard key={post?._id} post={post} />
+                    ))
+                ) : (
+                    <p className="no-results">No projects found</p>
+                )}
+            </ul>
         </section>
     </>
   );
