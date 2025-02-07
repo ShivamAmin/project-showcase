87bb71c22f9c2c1a4d289c42103b63c2f2f66e0b
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-02-06 23:42:26 -0700

    Add Search form to hero banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+    searchParams: Promise<{ query?: string }>
+}) {
+    const query = (await searchParams).query;
   return (
     <>
         <section className="pink_container">
@@ -8,6 +12,7 @@ export default function Home() {
             <p className="sub-heading !max-w-3xl">
                 Explore my projects and the technology behind them.
             </p>
+            <SearchForm query={query} />
         </section>
     </>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = async () => {
     return (
         <header className="px-5 py-3 bg-[a0a0a0] shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
-                <Link href="/">
+                <Link href="/public">
                     <Image src="/logo.svg" alt="logo" width={174.465} height={137.41} />
                 </Link>
                 <div className="flex items-center gap-5 text-white">
