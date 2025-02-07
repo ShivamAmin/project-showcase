942d6804cdb44921759eec1581bd0bbd3d7da2e5
Shivam Amin <11432922+ShivamAmin@users.noreply.github.com>
2025-02-06 23:42:27 -0700

    Implement Sanity Write token, and implement functionality to update project view count

--- components/Ping.tsx
+++ components/Ping.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Ping = () => {
     return (
         <div className={"relative"}>
-            <div className={"absolute -left-4 -top-1"}>
+            <div className={"absolute -left-4 top-1"}>
                 <span className={"flex size-[11px]"}>
                     <span className={"animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"} />
                     <span className={"relative inline-flex rounded-full size-[11px] bg-primary"} />
--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Ping from "@/components/Ping";
 import {client} from "@/sanity/lib/client";
+import {writeClient} from "@/sanity/lib/write-client";
 import {PROJECT_VIEWS_QUERY} from "@/sanity/lib/queries";
+import { after } from 'next/server';
 
 const View = async ({ id }: {id: string}) => {
     const { views: totalViews } = await client.withConfig({useCdn: false}).fetch(PROJECT_VIEWS_QUERY, {id});
-    //TODO: Update views when post is seen.
+    after(async() => (
+        await writeClient.patch(id).set({ views: totalViews + 1}).commit()
+    ));
     return (
         <div className={"view-container"}>
             <div className={"absolute -top-2 -right-2"}>
